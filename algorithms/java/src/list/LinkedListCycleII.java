0b83f5a2e54abc132315b5ff840e2f54b1bef6cd
shuanggan <qimiguang@gmail.com>
2018-08-22 16:01:39 +0800

    day-6

--- algorithms/java/src/list/LinkedListCycleII.java
+++ algorithms/java/src/list/LinkedListCycleII.java
@@ -14,6 +14,25 @@ public class LinkedListCycleII {
 
     public ListNode detectCycle(ListNode head) {
 
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (fast == slow) {
+                ListNode slow2 = head;
+                // 核心在于这里怎么理解
+                while (slow != slow2) {
+                    slow = slow.next;
+                    slow2 = slow2.next;
+                }
+
+                return slow;
+            }
+        }
+
         return null;
     }
 }
